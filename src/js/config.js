1138e513a2f522a818c44dae2b069b09863b3c4d
Chetan-B-B <chetanchetu9740@gmial.com>
2021-10-19 23:13:08 +0530

    Inital Commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+//used for having variables,  which has  the  important data about the application
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7f8a2446-09be-46d3-9d63-d4809377c230';
+export const MODAL_CLOSE_SEC = 2.5;
